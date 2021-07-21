dcb85e7e7c667319b933a491309e2892ac81da20
Cris Gonzalez <macristinagt@gmail.com>
2021-07-20 21:13:21 -0300

    problemaPromesa

---
+++ src/Components/data.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const servicios = [
+    
+    {
+        id:"1",
+        fotoUrl: "https://queautocompro.com/wp-content/uploads/2013/07/2009_Fit_Sport_101-1024x682.jpg",
+        nombre:"1nombre",
+        descripcion: "1descripcion",
+    },
+    {
+        id:"2",
+        fotoUrl: "https://queautocompro.com/wp-content/uploads/2013/07/2009_Fit_Sport_101-1024x682.jpg",
+        nombre:"2nombre",
+        descripcion: "2descripcion",
+    }
+]
